bba85435bddfc3474368e7990813ad3403dc90e2
woodyjohnson13 <johnsonwoody13@gmail.com>
2023-04-20 22:02:23 +0400

    Add main screen

--- User.py
+++ User.py
@@ -13,26 +13,4 @@ class User():
         print(f'Age:{self.age}')
         print(f'Gender:{self.gender}')
         
-#Child class
-class Bank(User):
-    def __init__(self,name,age,gender):
-        super().__init__(name,age,gender)
-        self.balance=0
-       
-    def deposit(self,amount):
-        self.balance+=amount
-        print(f'Amount Deposited:{amount}')
-        print(f'Balance:{self.balance}')
     
-    def withdraw(self,amount):
-        if amount>self.balance:
-            print("Insufficient Balance")
-        else:
-            self.balance-=amount
-            print(f'Amount Withdrawn:{amount}')
-            print(f'Balance:{self.balance}')   
-       
-       
-    def deatils(self):
-        super().deatils()
-        print(f'Balance:{self.balance}')
--- main.py
+++ main.py
@@ -1,8 +1,46 @@
-from User import User,Bank
+from bank import Bank
+from tkinter import *
+from PIL import  ImageTk,Image
+import os
 
-johny=Bank('John','18','male')
-johny.deatils()
-johny.deposit(500)
-johny.withdraw(1000)
-johny.withdraw(100)
-johny.deatils()
+
+
+
+def main():
+    
+    #main Screen
+    master=Tk()
+    master.title("OG Bank")
+    
+    #functions
+    def register():
+        #registration screen
+        register_screen=Toplevel(master)
+        register_screen.title('Register')
+        Label(register_screen, text='Please enter your details below to sighn up.', font=('Calibri,12')).grid(row=0,sticky=N,pady=10)         
+    
+    
+    def login():
+        print("This is login page")
+    
+    #image import 
+    img=Image.open('main_icon.png')
+    img=img.resize((150,150))
+    img=ImageTk.PhotoImage(img)
+    
+    #Labels
+    Label(master,text="Welcome to Og Bank",font=("Calibri",20)).grid(row=0,sticky=N,pady=10)
+    
+    Label(master,text="Most dope bank in the US",font=("Calibri",12)).grid(row=1,sticky=N)
+    
+    Label(master,image=img).grid(row=2,sticky=N,pady=15)
+    
+    #Buttons
+    Button(master,text='Register',font=('Calibri',12),width=20,command=register).grid(row=3,sticky=N)
+    
+    Button(master,text='Login',font=('Calibri',12),width=20,command=login).grid(row=4,pady=5,sticky=N)
+    
+    mainloop()
+
+if __name__ == "__main__":
+    main()
